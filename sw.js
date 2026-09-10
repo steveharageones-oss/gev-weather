f7f6df91f51bba38a73e057bb43a1984b20c50ac
Steve <steve.harageones@gmail.com>
2026-09-10 19:55:12 -0400

    Bump SW cache to v3: force installed PWAs onto alerts + 7-day-icon build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* God's Eye Weather service worker */
-var CACHE = 'gew-v2';
+var CACHE = 'gew-v3';
 var SHELL = ['./', './index.html', './manifest.json', './icon.svg', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
